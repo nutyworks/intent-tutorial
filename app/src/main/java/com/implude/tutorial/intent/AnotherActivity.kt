45841b73546d0ab779b18120c9690e926546d38b
김은교 <65056583+eks05@users.noreply.github.com>
2021-06-09 15:40:46 +0900

    dd

--- app/src/main/java/com/implude/tutorial/intent/AnotherActivity.kt
+++ app/src/main/java/com/implude/tutorial/intent/AnotherActivity.kt
@@ -1,20 +1,19 @@
 package com.implude.tutorial.intent
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 
-class MainActivity : AppCompatActivity() {
+class AnotherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.another_activity)
 
-        var a = findViewById<Button>(R.id.btn_open)
+        var a = findViewById<Button>(R.id.a)
         a.setOnClickListener{
-            val b = Intent(this, AnotherActivity::class.java)
-            startActivity(b)
+            findViewById<TextView>(R.id.b).text = "Hi"
         }
     }
 }
