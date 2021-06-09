015963839c874bf222b4cc7cf87b06713e841f3d
fhfhfhfhgpdl <81856603+fhfhfhfhgpdl@users.noreply.github.com>
2021-06-09 16:48:10 +0900

    commit

--- app/src/main/java/com/implude/tutorial/intent/MainActivity.kt
+++ app/src/main/java/com/implude/tutorial/intent/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.implude.tutorial.intent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_open=findViewById<Button>(R.id.btn_open)
+
+        btn_open.setOnClickListener{
+            startActivity(Intent(this, AnotherActivity::class.java))
+        }
     }
+
 }
+
+
+
