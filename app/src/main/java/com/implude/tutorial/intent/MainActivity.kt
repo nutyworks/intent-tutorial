82e19f144b9bc4550141b92b36c88e359d176765
fhfhfhfhgpdl <81856603+fhfhfhfhgpdl@users.noreply.github.com>
2021-06-10 15:37:20 +0900

    commit

--- app/src/main/java/com/implude/tutorial/intent/MainActivity.kt
+++ app/src/main/java/com/implude/tutorial/intent/MainActivity.kt
@@ -11,7 +11,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btn_open=findViewById<Button>(R.id.btn_open)
-        val a=1
 
         btn_open.setOnClickListener{
             startActivity(Intent(this, AnotherActivity::class.java))
