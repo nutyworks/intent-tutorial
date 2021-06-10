2ec2e52864a9e4e9b93279194c22f948fd73f370
redstonecube <superhoy0509@gmail.com>
2021-06-10 20:53:46 +0900

    feat:open another activity on button click

---
+++ app/src/main/java/com/implude/tutorial/intent/anotheractivity.kt
@@ -0,0 +1,11 @@
+package com.implude.tutorial.intent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class anotheractivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_anotheractivity)
+    }
+}
