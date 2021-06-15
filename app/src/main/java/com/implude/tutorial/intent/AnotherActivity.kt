c49f8604eae6799e2d701b1ad84001b0d1fcb219
fhfhfhfhgpdl <81856603+fhfhfhfhgpdl@users.noreply.github.com>
2021-06-15 21:33:34 +0900

    Revert "intend-tutorial"

--- app/src/main/java/com/implude/tutorial/intent/AnotherActivity.kt
+++ app/src/main/java/com/implude/tutorial/intent/AnotherActivity.kt
@@ -1,19 +1,13 @@
 package com.implude.tutorial.intent
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
 class AnotherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_another)
 
-        val go_back=findViewById<Button>(R.id.go_back)
 
-        go_back.setOnClickListener{
-            startActivity(Intent(this, MainActivity::class.java))
-        }
     }
 }
